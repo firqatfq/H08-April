2ae7e143090412e28287b567c9d7286fa95630d8
firqatfq <firqatfq@code.edu.az>
2023-04-10 00:11:15 +0400

    Added my homework for April 8

---
+++ H08-April/Program.cs
@@ -0,0 +1,13 @@
+﻿//Student student = new Student();
+
+//student.Name = "Firgat";
+//student.SurName = "Gasimov";
+//student.Group = "P234";
+//student.Point = 99;
+//student.IsGraduated = false;
+
+Student student = new Student("Firgat", "Gasimov", "P234", 99, false);
+
+student.GetFullName();
+student.GetAllInfo();
+student.CheckPoint();
---
+++ H08-April/Student.cs
@@ -0,0 +1,59 @@
+﻿internal class Student
+    {
+
+    #region Fields
+
+    public string Name;
+    public string SurName;
+    public string Group;
+    public int Point;
+    public bool IsGraduated;
+
+    #endregion
+
+    #region Methods
+
+    public void GetFullName()
+    {
+        Console.WriteLine(Name + " " + SurName);
+    }
+    public void GetAllInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Surname: {SurName}");
+        Console.WriteLine($"Group: {Group}");
+        Console.WriteLine($"Point: {Point}");
+        Console.WriteLine($"isGraduated: {IsGraduated}");
+        if (IsGraduated) Console.WriteLine("This student has graduated.");
+        else Console.WriteLine("Still learning...");
+    }
+    public void CheckPoint()
+    {
+        if (Point > 80) Console.WriteLine("You can proceed to the next exam.");
+        else Console.WriteLine("You need to pass the previous exam.");
+    }
+
+    #endregion
+
+    #region Constructors
+    public Student():this("Someone", "Random")
+    {
+
+    }
+    public Student(string name, string surname)
+    {
+        this.Name = name;
+        this.SurName = surname;
+    }
+    public Student(string name, string surname, string group):this(name, surname)
+    {
+        this.Group = group;
+    }
+    public Student(string name, string surname, string group, int point, bool isGraduated):this(name, surname, group)
+    {
+        this.Point = point;
+        this.IsGraduated = isGraduated;
+    }
+    #endregion
+
+}
